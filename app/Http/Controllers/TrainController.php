82a55085bb0e5503a9264f300eb050ebac4e1028
MattiaBologna <mattiabologna01@gmail.com>
2024-05-27 18:01:04 +0200

    TrainController and Model

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Train;
+
+
+class TrainController extends Controller
+{
+    public function index() 
+    {
+
+        // recuperare i treni in partenza oggi 
+        $trains = Train::all();
+        dd($trains);
+
+        // ritornare la vista home con i treni 
+        return view('home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TrainController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [TrainController::class, 'index'])->name('home') ;
+
+
